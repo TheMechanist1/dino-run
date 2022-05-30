3c4fac3d0104f2df7d17f51e3c37c269c53f574c
TheMechanist1 <dminer625@gmail.com>
2022-05-30 17:57:49 -0500

    Jump

--- main.py
+++ main.py
@@ -2,33 +2,66 @@ from time import sleep
 from machine import Pin, SPI
 from ssd1309 import Display
 
+spi = SPI(0, baudrate=14500000, sck=Pin(18), mosi=Pin(19))
+display = Display(spi, dc=Pin(16), cs=Pin(17), rst=Pin(20))
+button = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)
+
 class Main:
     def __init__(self):
-        print(d.speed)
-        self.spi = SPI(0, baudrate=14500000, sck=Pin(18), mosi=Pin(19))
-        self.display = Display(self.spi, dc=Pin(16), cs=Pin(17), rst=Pin(20))
+        self.player = Dino(0, 0, 0)
+        
         
     def loop(self):
-        pass
+        self.player.loop()
 
     def draw(self):
-        self.display.draw_bitmap("images/DinoStand1.mono", 0, self.display.height - 22, 20, 22)
-        self.display.present()
+        self.player.draw()
         
-        self.display.draw_bitmap("images/DinoStand2.mono", 0, self.display.height - 22, 20, 22)
-        self.display.present()
 
 class Dino:
-    def __init__(self, score, speed):
+    def __init__(self, score, speed, y):
         self.score = score
         self.speed = speed
+        self.y = y
+        self.velY = y
+        self.accY = -0.1
+        
+    def loop(self):
+        self.velY += self.accY
+        if ((self.y + self.velY) >= 0):
+            self.y += self.velY
+            display.clear()
+        else:
+            self.velY = 0
+            self.y = 0
+        
+        if button.value() == 0 and self.velY < 1 and self.y <= 0.1:
+            self.velY += 3
+            print(self.y)
+            
+    
+    def draw(self):
+        display.draw_bitmap("images/DinoStand1.mono", 0, display.height - 22 - int(self.y), 20, 22)
+        display.present()
+        
+        display.draw_bitmap("images/DinoStand2.mono", 0, display.height - 22 - int(self.y), 20, 22)
+        display.present()
         
 class Obstacle:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-d = Dino(0, 0)
+        
+class Vector2d:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def getX():
+        return self.x
+    
+    def getY():
+        return self.y
 
 if __name__ == "__main__":
     m = Main();
