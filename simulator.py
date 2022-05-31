f4518fde5fadc3ca156b0c57e09da4ebbcc6e82d
Thomas Weber <muffin@muffin.ink>
2022-05-31 15:19:27 -0500

    simulator: Implement display

--- simulator.py
+++ simulator.py
@@ -1,17 +1,42 @@
 import pygame
+from PIL import Image
 import sys
 import main
-
-pygame.init()
 
 # https://www.orientdisplay.com/wp-content/uploads/2021/05/AOM12864A0-1.54WW-ANO.pdf
 width = 128
 height = 64
 scale = 5
 
-def parse_mono_image(path):
+# TODO: main should be managing framerate on its own because this has to be the same everywhere
+framerate = 60
+
+def parse_mono_image(path, width, height):
     with open(path, "rb") as f:
-        print(f.read())
+        mono = f.read()
+        rgb = []
+        current_x = 0
+        for i in mono:
+            for j in range(8):
+                current_x += 1
+                if current_x > width:
+                    current_x = 0
+                    break
+                bit = i & (1 << j)
+                if bit == 0:
+                    rgb.append(0)
+                    rgb.append(0)
+                    rgb.append(0)
+                else:
+                    rgb.append(255)
+                    rgb.append(255)
+                    rgb.append(255)
+
+    image = Image.frombytes('RGB', (width, height), bytes(rgb))
+    scaled_width = width * scale
+    scaled_height = height * scale
+    resized_image = image.resize((scaled_width, scaled_height), resample=Image.NONE)
+    return pygame.image.frombuffer(resized_image.tobytes(), (scaled_width, scaled_height), "RGB")
 
 class SimulatedDisplay:
     def __init__(self):
@@ -20,7 +45,8 @@ class SimulatedDisplay:
         self.screen = pygame.display.set_mode((width * scale, height * scale))
 
     def draw_bitmap(self, path, x, y, width, height):
-        pass
+        image = parse_mono_image(path, width, height)
+        self.screen.blit(image, (x * scale, y * scale))
 
     def present(self):
         pygame.display.flip()
@@ -36,10 +62,17 @@ class SimulatedButton:
         return 0
 
 def run():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
     main.display = SimulatedDisplay()
     main.button = SimulatedButton()
     m = main.Main()
+
     while True:
+        clock.tick(framerate)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
