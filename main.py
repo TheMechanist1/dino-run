0acc23909249089adc9599a1c6f0101c56fe00eb
Mechanist <dminer625@gmail.com>
2022-06-02 13:16:52 -0500

    Switched to snake_case

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Main:
         self.player = Dino()
         self.obs = [Obstacle(0)]
         self.end = False
-        self.startTime = time.ticks_ms()
+        self.start_time = time.ticks_ms()
         
         
     def reset(self):
@@ -26,9 +26,9 @@ class Main:
         self.player = Dino()
         self.obs = [Obstacle(0)]
         self.end = False
-        self.startTime = time.ticks_ms()
+        self.start_time = time.ticks_ms()
     
-    def mainGameLoop(self):
+    def main_game_loop(self):
         while self.end != True:
             preframe = time.ticks_ms()
             self.loop()
@@ -39,15 +39,15 @@ class Main:
             self.reset()
             self.end = False
         
-    def detectCollision(self, dino, obs):
-        return (dino.x < obs.x + obs.imgWidth and dino.x + dino.imgWidth > obs.x and
-                dino.y < obs.y + obs.imgHeight and dino.imgHeight + dino.y > obs.y)
+    def detect_collision(self, dino, obs):
+        return (dino.x < obs.x + obs.img_width and dino.x + dino.img_width > obs.x and
+                dino.y < obs.y + obs.img_height and dino.img_height + dino.y > obs.y)
         
     def loop(self):
         self.player.loop()
-        self.player.score = time.ticks_diff(time.ticks_ms(), self.startTime)
+        self.player.score = time.ticks_diff(time.ticks_ms(), self.start_time)
         for o in self.obs:
-            if self.detectCollision(self.player, o):
+            if self.detect_collision(self.player, o):
                 print("bruh")
                 self.obs.pop(0)
                 self.end = True
@@ -55,9 +55,9 @@ class Main:
             o.speed = self.player.speed
             if o.isOffScreen():
                 self.obs.pop(0)
-                newOb = Obstacle(int(random.uniform(0,2)))
-                newOb.x += random.uniform(0,5) + 30
-                self.obs.append(newOb)
+                new_obs = Obstacle(int(random.uniform(0,2)))
+                new_obs.x += random.uniform(0,5) + 30
+                self.obs.append(new_obs)
             o.loop()
 
     def draw(self):
@@ -73,23 +73,23 @@ class Dino:
     def __init__(self):
         self.score = 0
         self.speed = 1.5
-        self.velY = 0
-        self.accY = -0.1
-        self.imgWidth = 22
-        self.imgHeight = 24
+        self.vel_y = 0
+        self.acc_y = -0.1
+        self.img_width = 22
+        self.img_height = 24
         self.x = 0
         self.y = 0
         
     def loop(self):
-        self.velY += self.accY
-        if ((self.y + self.velY) >= 0):
-            self.y += self.velY
+        self.vel_y += self.acc_y
+        if ((self.y + self.vel_y) >= 0):
+            self.y += self.vel_y
         else:
-            self.velY = 0
+            self.vel_y = 0
             self.y = 0
         
-        if button.value() == 0 and self.velY < 1 and self.y <= 0.1:
-            self.velY += 3
+        if button.value() == 0 and self.vel_y < 1 and self.y <= 0.1:
+            self.vel_y += 3
         
         if(self.score%10000 <= 16):
             self.speed += 0.25
@@ -98,31 +98,31 @@ class Dino:
     def draw(self):
         global frames
         if frames%10 < 5:
-            display.draw_bitmap("images/DinoStand0.mono", 0, display.height - self.imgHeight - int(self.y), self.imgWidth, self.imgHeight)
+            display.draw_bitmap("images/DinoStand0.mono", 0, display.height - self.img_height - int(self.y), self.img_width, self.img_height)
         else:
-            display.draw_bitmap("images/DinoStand1.mono", 0, display.height - self.imgHeight - int(self.y), self.imgWidth, self.imgHeight)
+            display.draw_bitmap("images/DinoStand1.mono", 0, display.height - self.img_height - int(self.y), self.img_width, self.img_height)
         
-        scoreWidth = bally.measure_text(str(self.score))
-        speedWidth = bally.measure_text(str(self.speed))
-        display.draw_text(display.width - scoreWidth, 0, str(self.score), bally)
-        display.draw_text(display.width - speedWidth, 17, str(self.speed), bally)
+        score_width = bally.measure_text(str(self.score))
+        speed_width = bally.measure_text(str(self.speed))
+        display.draw_text(display.width - score_width, 0, str(self.score), bally)
+        display.draw_text(display.width - speed_width, 17, str(self.speed), bally)
         
 class Obstacle:
-    def __init__(self, obsType):
-        if obsType == 0:
-            self.imgPath = "images/obs-0.mono"
-            self.imgWidth = 24
-            self.imgHeight = 25
-        elif obsType == 1:
-            self.imgPath = "images/obs-1.mono"
-            self.imgWidth = 24
-            self.imgHeight = 25
-        elif obsType == 2:
-            self.imgPath = "images/obs-2.mono"
-            self.imgWidth = 24
-            self.imgHeight = 25
-        elif obsType > 2:
-            raise Exception("Obstacale Type should be 0-2. You put " + str(obsType))
+    def __init__(self, obs_type):
+        if obs_type == 0:
+            self.img_path = "images/obs-0.mono"
+            self.img_width = 24
+            self.img_height = 25
+        elif obs_type == 1:
+            self.img_path = "images/obs-1.mono"
+            self.img_width = 24
+            self.img_height = 25
+        elif obs_type == 2:
+            self.img_path = "images/obs-2.mono"
+            self.img_width = 24
+            self.img_height = 25
+        elif obs_type > 2:
+            raise Exception("Obstacale Type should be 0-2. You put " + str(obs_type))
         else:
             raise Exception("Bro what are you doing?")
         
@@ -131,16 +131,15 @@ class Obstacle:
         self.speed = 1.5
     
     def isOffScreen(self):
-        return self.x <= 0 - self.imgWidth
+        return self.x <= 0 - self.img_width
     
     def loop(self):
         self.x -= self.speed
-        pass
 
     def draw(self):
-        display.draw_bitmap(self.imgPath, int(self.x), display.height - self.imgHeight, self.imgWidth, self.imgHeight)
+        display.draw_bitmap(self.img_path, int(self.x), display.height - self.img_height, self.img_width, self.img_height)
 
 if __name__ == '__main__':
     m = Main()
     while True:
-        m.mainGameLoop()
+        m.main_game_loop()
